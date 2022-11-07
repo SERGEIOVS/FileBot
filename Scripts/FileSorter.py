b544ea4570b978ec83e825f9029b7add22f068e3
sergeiovs <mr.serz98@mail.ru>
2022-11-06 16:01:00 -0800

    13

--- Scripts/FileSorter.py
+++ Scripts/FileSorter.py
@@ -1,30 +1,21 @@
 import os,shutil,sys
 
-mydir = os.listdir('My1/')
+mydir1 = os.listdir('C:\\123456789\\videos\Myfoto1/')
 
-mydir0 =os.path.basename(mydir[0])
-
+FileTypes=[
+[]    
+]
 run = True
-
 while run:
-    
+
             menupointslist = [
-
 ' - Создать  папку / несколько папок' ,
-
 ' - Выбрать нужную папку' ,
-
 ' - Удалить нужную папку / несколько папок' ,
-
 ' - Переименовать нужную папку / несколько папок' ,
-
 ' - Создать файл / несколько файлов' ,
-
 ' - Показать содержимое  нужной папки' ,
-
-' - Показать содержимое нужного файла'
-            
-            ]
+' - Показать содержимое нужного файла']
 
             print()
             print('----')
@@ -34,55 +25,37 @@ while run:
             print('----')
 
             for i in range( len( menupointslist ) ) :
-                
                 print( i + 1 , menupointslist[i]  )
-            
+
+            print('-------------------------------------------------------------------')
+            print()
+            action = input('Enter an action : ')
+            if action == 'count':
+                directory1 = input('Enter a directory old name:')
+                filetype1 = input('Enter a file type:')
+
                 print()
+            
+                for i in range(len(mydir1)):
+                        if filetype1 in mydir1[i] and filetype1 == 'Screenshot':
+                            i = os.path.basename(mydir1[i])
+                            FileTypes[0].append(i)
+                            #print(screenshotlist)
+                            #print('name = ' , i )
 
-            print('----')
+                print('Elements total : ' , len(mydir1))
+                print(filetype1 , "'s : " , len(FileTypes[0]))
 
-            print()
+                if filetype1 in mydir1[i] and filetype1 == 'IMG':
+                            i = os.path.basename(mydir1[i])
+                            FileTypes[0].append(i)
+                            #print(screenshotlist)
+                            #print('name = ' , i )
 
-            directory = input('directory : ')
+                print('Elements total : ' , len(mydir1))
+                print(filetype1 , "'s : " , len(FileTypes[0]))
 
-            print()
+print()
+print()
 
-            print('Elements : ' , len(directory))
-
-            print()
-
-            print('mydir  =  '  , mydir)
-
-            print()
-
-            print()
-
-            print('----------------------------------------------------------------------------------------------------------')
-
-            """            name = input('name : ')
-
-                        obj = open(str(directory) + name)
-
-                        new_name = input('new  name : ')
-
-                        directory = input('new  directory : ')
-
-                        obj.save(str(directory) + new_name )"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('----------------------------------------------------------------------------------------------------------')
